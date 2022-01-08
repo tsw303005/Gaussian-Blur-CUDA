f3a02315c62b10c3751405c8a2eab4c4d5dff167
tsw303005 <tsw303005@gmail.com>
2022-01-08 17:19:13 +0800

    feature: Multiple CPU done

--- src/Multiple-CPU/gaussian_blur.cpp
+++ src/Multiple-CPU/gaussian_blur.cpp
@@ -4,7 +4,6 @@
 #include <png.h>
 #include <algorithm>
 #include <omp.h>
-#include <string.h>
 
 #define PI std::acos(-1)
 
@@ -86,20 +85,15 @@ void gaussian_filter(const unsigned r, double **filter_matrix, double *wsum) {
 
 void gaussian_blur(unsigned char **src_image, unsigned char **tar_image, const unsigned height, const unsigned width,
                 const unsigned channels, const unsigned r, double **filter_matrix, double wsum) {
-    int rs = ceil((double)r * 2.57);
-    int x, y;
     cpu_set_t cpu_set;
     sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
     int ncpus = CPU_COUNT(&cpu_set);
 
     #pragma omp parallel num_threads(ncpus) shared(src_image, tar_image, wsum, filter_matrix, channels, width, height)
     {
+        int rs = ceil((double)r * 2.57);
+        int x, y;
         double val[channels];
-        unsigned char *src = (unsigned char*)malloc(sizeof(unsigned char) * height * width * channels);
-        memcpy(src, *src_image, channels * height * width * sizeof(unsigned char));
-        double *filter = (double*)malloc((2 * rs + 1) * (2 * rs + 1) * sizeof(double));
-        memcpy(filter, *filter_matrix, (2 * rs + 1) * (2 * rs + 1) * sizeof(double));
-
         for (int i = omp_get_thread_num(); i < height; i+=omp_get_num_threads()) {
             //std::cout<< omp_get_thread_num() << ' ' << i << std::endl;
             for (int j = 0; j < width; j++) {
@@ -110,9 +104,9 @@ void gaussian_blur(unsigned char **src_image, unsigned char **tar_image, const u
                         x = std::min((int)width-1, std::max(0, ix));
                         y = std::min((int)height-1, std::max(0, iy));
 
-                        val[0] += src[channels * (y * width + x) + 0] * filter[(2*rs+1)*a + b];
-                        val[1] += src[channels * (y * width + x) + 1] * filter[(2*rs+1)*a + b];
-                        val[2] += src[channels * (y * width + x) + 2] * filter[(2*rs+1)*a + b];
+                        val[0] += (*src_image)[channels * (y * width + x) + 0] * (*filter_matrix)[(2*rs+1)*a + b];
+                        val[1] += (*src_image)[channels * (y * width + x) + 1] * (*filter_matrix)[(2*rs+1)*a + b];
+                        val[2] += (*src_image)[channels * (y * width + x) + 2] * (*filter_matrix)[(2*rs+1)*a + b];
                     }
                 }
                 for (unsigned now = 0; now < channels; now++) (*tar_image)[channels * (i * width + j) + now] = round(val[now] / wsum);
